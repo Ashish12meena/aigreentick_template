49c7143712ef2674127c1cb73d861c8535b4f957
Ashish meena <ashishmeena100603@gmail.com>
2026-01-16 17:07:43 +0530

    update it

--- src/main/java/com/aigreentick/services/template/model/contact/ContactAttributes.java
+++ src/main/java/com/aigreentick/services/template/model/contact/ContactAttributes.java
@@ -11,7 +11,7 @@ public class ContactAttributes {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "contact_id", nullable = false)
